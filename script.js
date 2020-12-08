c5e145ccf5625eb59017d29887201c2088b5a92c
Dane Hartman <dhartman@jeffcoschools.us>
2020-12-08 01:53:35 +0000

    Initial Version

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ function appendItem(listName, item) {
   listName.push(item);
 }
 
+function insertItem(listName, index, item) {
+  listName.splice(index, 0, item);
+}
+
+function removeItem(listName, index) {
+  listName.splice(index, 1);
+}
+
 var bList = ["to", 5, "po"];
 appendItem(bList, bList[2] + "ta" + bList[0]); 
-//insertItem(bList, 2-1, "go");
-//removeItem(bList, 2);
+insertItem(bList, 2-1, "go");
+removeItem(bList, 2);
 console.log(bList);
