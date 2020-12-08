b94b7c2ccd44b330ffc04adadd1baf5e6f277991
Dane Hartman <dhartman@jeffcoschools.us>
2020-12-08 02:04:40 +0000

    Write to HTML element

--- script.js
+++ script.js
@@ -11,7 +11,10 @@ function removeItem(listName, index) {
 }
 
 var bList = ["to", 5, "po"];
+var title = document.getElementById("titleText");
+title.innerHTML = "Hello!";
 appendItem(bList, bList[2] + "ta" + bList[0]); 
 insertItem(bList, 2-1, "go");
 removeItem(bList, 2);
 console.log(bList);
+//document.getElementById("titleText").innerHTML = "New text!";
